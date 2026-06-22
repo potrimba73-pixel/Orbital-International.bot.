6e7d97ec770dbaa8ee5a4bbb5ab21ba87bec57a2
potrimba73-pixel <potrimba73@gmail.com>
2026-06-22 15:47:06 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,34 +1,24 @@
 const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
-const UserProfile = require('../utils/models/UserProfile');
 const i18n = require('../utils/i18n');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('help')
-    .setDescription('Show help menu'),
+    .setDescription('Show available commands'),
 
   async execute(interaction) {
-    let lang = 'en';
-
-    try {
-      const profile = await UserProfile.findOne({ userId: interaction.user.id });
-      if (profile && profile.nativeLanguage) {
-        lang = profile.nativeLanguage;
-      }
-    } catch (e) {
-      // Database error, use default English
-    }
+    const lang = await i18n.getUserLang(interaction.user.id);
 
     const embed = new EmbedBuilder()
-      .setTitle('🌍 ' + i18n.get(lang, 'help.title'))
+      .setTitle(i18n.get(lang, 'help.title'))
       .setDescription(i18n.get(lang, 'help.desc'))
       .setColor(0x5865F2)
       .addFields(
-        { name: '🎓 ' + i18n.get(lang, 'help.language_title'), value: i18n.get(lang, 'help.language_desc'), inline: false },
-        { name: '📊 ' + i18n.get(lang, 'help.stats_title'), value: i18n.get(lang, 'help.stats_desc'), inline: false },
-        { name: '⚙️ ' + i18n.get(lang, 'help.general_title'), value: i18n.get(lang, 'help.general_desc'), inline: false }
+        { name: i18n.get(lang, 'help.lang_title'), value: i18n.get(lang, 'help.lang_cmds'), inline: false },
+        { name: i18n.get(lang, 'help.social_title'), value: i18n.get(lang, 'help.social_cmds'), inline: false },
+        { name: i18n.get(lang, 'help.utils_title'), value: i18n.get(lang, 'help.utils_cmds'), inline: false }
       )
-      .setFooter({ text: i18n.get(lang, 'help.footer') })
+      .setFooter({ text: 'Orbital International - Help' })
       .setTimestamp();
 
     await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
