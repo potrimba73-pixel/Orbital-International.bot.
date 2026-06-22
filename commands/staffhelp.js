040b4edd73fb37f70273169c513503f7bb33211d
potrimba73-pixel <potrimba73@gmail.com>
2026-06-22 15:45:27 +0100

    Update staffhelp.js

--- commands/staffhelp.js
+++ commands/staffhelp.js
@@ -4,23 +4,23 @@ const i18n = require('../utils/i18n');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('staffhelp')
-    .setDescription('Staff commands help menu')
+    .setDescription('Show staff commands')
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
   async execute(interaction) {
     const lang = await i18n.getUserLang(interaction.user.id);
 
     const embed = new EmbedBuilder()
-      .setTitle('🛡️ ' + i18n.get(lang, 'staffhelp.title'))
+      .setTitle(i18n.get(lang, 'staffhelp.title'))
       .setDescription(i18n.get(lang, 'staffhelp.desc'))
       .setColor(0xED4245)
       .addFields(
-        { name: '📋 ' + i18n.get(lang, 'staffhelp.mod_title'), value: i18n.get(lang, 'staffhelp.mod_desc'), inline: false },
-        { name: '🔧 ' + i18n.get(lang, 'staffhelp.setup_title'), value: i18n.get(lang, 'staffhelp.setup_desc'), inline: false },
-        { name: '👤 ' + i18n.get(lang, 'staffhelp.user_title'), value: i18n.get(lang, 'staffhelp.user_desc'), inline: false },
-        { name: '⚙️ ' + i18n.get(lang, 'staffhelp.bot_title'), value: i18n.get(lang, 'staffhelp.bot_desc'), inline: false }
+        { name: i18n.get(lang, 'staffhelp.mod_title'), value: i18n.get(lang, 'staffhelp.mod_cmds'), inline: false },
+        { name: i18n.get(lang, 'staffhelp.setup_title'), value: i18n.get(lang, 'staffhelp.setup_cmds'), inline: false },
+        { name: i18n.get(lang, 'staffhelp.user_title'), value: i18n.get(lang, 'staffhelp.user_cmds'), inline: false },
+        { name: i18n.get(lang, 'staffhelp.bot_title'), value: i18n.get(lang, 'staffhelp.bot_cmds'), inline: false }
       )
-      .setFooter({ text: i18n.get(lang, 'staffhelp.footer') })
+      .setFooter({ text: 'Orbital International - Staff Panel' })
       .setTimestamp();
 
     await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
